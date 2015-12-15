f680ce0db37a39b41e24ba556f649d3a8cd9db94
Ordiel <ordiel1910@gmail.com>
2015-12-15 13:11:06 -0700

    **DBCreator.sql** \n * Added script to create the schema sckeleton

---
+++ DBCreator.sql
@@ -0,0 +1,51 @@
+DROP SCHEMA drae;
+
+CREATE SCHEMA IF NOT EXISTS drae;
+
+USE drae;
+
+CREATE TABLE atributo (
+		palabra TEXT NOT NULL COLLATE utf8_bin,
+        uso TINYINT not null,
+        atributo TEXT NOT NULL
+	);
+    
+CREATE TABLE etimologia (
+		palabra TEXT NOT NULL COLLATE utf8_bin,
+        p_etimologica TEXT NOT NULL COLLATE utf8_bin,
+        foranea BOOLEAN NOT NULL
+	);
+    
+CREATE TABLE femenina (
+		palabra TEXT NOT NULL COLLATE utf8_bin
+	);
+    
+CREATE TABLE genesis (
+		palabra TEXT NOT NULL COLLATE utf8_bin,
+        origen TEXT NOT NULL
+	);
+    
+CREATE TABLE masculina (
+		palabra TEXT NOT NULL COLLATE utf8_bin
+	);
+CREATE TABLE palabra (
+		palabra TEXT NOT NULL COLLATE utf8_bin,
+        texto TEXT NOT NULL COLLATE utf8_bin
+	);
+    
+CREATE TABLE palabra_multigenero (
+		palabra TEXT NOT NULL COLLATE utf8_bin,
+        variante TEXT NOT NULL COLLATE utf8_bin,
+        diferencia TEXT NOT NULL COLLATE utf8_bin
+	);
+ 
+CREATE TABLE signf_o_var_pal_foranea (
+		palabra TEXT NOT NULL COLLATE utf8_bin,
+        significado_variante TEXT NOT NULL COLLATE utf8_bin
+	);
+    
+CREATE TABLE variante (
+		palabra TEXT NOT NULL COLLATE utf8_bin,
+        variante TEXT NOT NULL COLLATE utf8_bin
+	);
+    
